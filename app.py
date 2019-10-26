2ea7ee8f0febd309ea1ecb5a0009a647753c9087
Ryan Holt <ryan.holt.rmh@gmail.com>
2019-10-26 10:30:37 -0600

    Added template dash

---
+++ app.py
@@ -0,0 +1,118 @@
+# Import required libraries
+import pickle
+import copy
+import pathlib
+import dash
+import math
+import datetime as dt
+import pandas as pd
+from dash.dependencies import Input, Output, State, ClientsideFunction
+import dash_core_components as dcc
+import dash_html_components as html
+
+# get relative data folder
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("data").resolve()
+
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+server = app.server
+
+
+# Load data
+df = pd.read_csv(DATA_PATH.joinpath("wellspublic.csv"), low_memory=False)
+df["Date_Well_Completed"] = pd.to_datetime(df["Date_Well_Completed"])
+df = df[df["Date_Well_Completed"] > dt.datetime(1960, 1, 1)]
+
+trim = df[["API_WellNo", "Well_Type", "Well_Name"]]
+trim.index = trim["API_WellNo"]
+dataset = trim.to_dict(orient="index")
+
+points = pickle.load(open(DATA_PATH.joinpath("points.pkl"), "rb"))
+
+
+# Create global chart template
+mapbox_access_token = "pk.eyJ1IjoiamFja2x1byIsImEiOiJjajNlcnh3MzEwMHZtMzNueGw3NWw5ZXF5In0.fk8k06T96Ml9CLGgKmk81w"
+
+layout = dict(
+    autosize=True,
+    automargin=True,
+    margin=dict(l=30, r=30, b=20, t=40),
+    hovermode="closest",
+    plot_bgcolor="#F9F9F9",
+    paper_bgcolor="#F9F9F9",
+    legend=dict(font=dict(size=10), orientation="h"),
+    title="Satellite Overview",
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        style="light",
+        center=dict(lon=-78.05, lat=42.54),
+        zoom=7,
+    ),
+)
+
+# Create app layout
+
+
+app.layout = html.Div(
+    [
+        dcc.Store(id="aggregate_data"),
+        # empty Div to trigger javascript file for graph resizing
+        html.Div(id="output-clientside"),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.Img(
+                            src=app.get_asset_url("dash-logo.png"),
+                            id="plotly-image",
+                            style={
+                                "height": "60px",
+                                "width": "auto",
+                                "margin-bottom": "25px",
+                            },
+                        )
+                    ],
+                    className="one-third column",
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                html.H3(
+                                    "New York Oil and Gas",
+                                    style={"margin-bottom": "0px"},
+                                ),
+                                html.H5(
+                                    "Production Overview", style={"margin-top": "0px"}
+                                ),
+                            ]
+                        )
+                    ],
+                    className="one-half column",
+                    id="title",
+                ),
+                html.Div(
+                    [
+                        html.A(
+                            html.Button("Learn More", id="learn-more-button"),
+                            href="https://plot.ly/dash/pricing/",
+                        )
+                    ],
+                    className="one-third column",
+                    id="button",
+                ),
+            ],
+            id="header",
+            className="row flex-display",
+            style={"margin-bottom": "25px"},
+        ),
+    ],
+    id="mainContainer",
+    style={"display": "flex", "flex-direction": "column"},
+)
+
+# Main
+if __name__ == "__main__":
+    app.run_server(debug=True)
