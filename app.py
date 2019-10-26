50341e18f6bacea1256245c11758ba489391deca
Ryan Holt <ryan.holt.rmh@gmail.com>
2019-10-26 12:33:09 -0600

    Added upload

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import pandas as pd
 from dash.dependencies import Input, Output, State, ClientsideFunction
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_helper
+import itertools
 
 # get relative data folder
 PATH = pathlib.Path(__file__).parent
@@ -16,7 +18,6 @@ app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
 )
 server = app.server
-
 
 layout = dict(
     autosize=True,
@@ -37,6 +38,10 @@ app.layout = html.Div(
         dcc.Store(id="aggregate_data"),
         # empty Div to trigger javascript file for graph resizing
         html.Div(id="output-clientside"),
+        html.Div(
+            id="output-data-upload",
+            style={"display": "none"}
+        ),
         html.Div(
             [
                 html.Div(
@@ -80,6 +85,16 @@ app.layout = html.Div(
                     className="one-third column",
                     id="button",
                 ),
+                html.Div(
+                    [
+                        dcc.Upload(
+                            id="upload-data",
+                            multiple=True,
+                            children=html.Button(
+                                "Upload CSVs")
+                        ),
+                    ],
+                ),
             ],
             id="header",
             className="row flex-display",
@@ -89,6 +104,36 @@ app.layout = html.Div(
     id="mainContainer",
     style={"display": "flex", "flex-direction": "column"},
 )
+
+@app.callback(
+        Output("output-data-upload", "children"),
+        [Input("upload-data", "contents"),
+         Input("upload-data", "filename")],
+        [State("output-data-upload", "children")],
+    )
+def upload(contents, filenames, list_of_ds):
+    """ Handles upload and creation of CSVs"""
+    # Dash elements default to None but returning None will break callbacks
+    if list_of_ds is None:
+        list_of_ds = []
+    # Check that contents has content
+    if contents is None:
+        return list_of_ds
+    # Check that there is no duplicates in the filenames of the upload
+    if dash_helper.duplicate_filename_check(filenames):
+        return list_of_ds
+
+    children = [dash_helper.parse_contents(c, f) for c, f in zip(contents, filenames)]
+
+
+    children, filenames = dash_helper.remove_bad_files(children, filenames)
+    filenames = dash_helper.remove_file_extension(filenames)
+    if not children:
+        return list_of_ds
+    if list_of_ds is not None:
+        children = list_of_ds + children
+    return children
+
 
 # Main
 if __name__ == "__main__":
---
+++ dash_helper.py
@@ -0,0 +1,99 @@
+"""Helper functions for dash layout and callbacks."""
+import base64
+import io
+import re
+
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+import pandas as pd
+
+
+def parse_contents(contents, filename):
+    df_json = []
+    filename_valid, filename = _check_filename(filename)
+    if filename_valid is False:
+        return None
+
+    try:
+        df = _get_ds_dataframe(contents)
+        df_json = df.to_json(orient="split", date_format="iso", date_unit="ns")
+
+    except Exception as e:
+        print(e)
+        print("There was an error processing this file.")
+
+    return df_json
+
+
+def read_json(json_list, enabled, date_columns=None):
+    df_list = []
+    try:
+        for json, enabled in zip(json_list, enabled):
+            if enabled == 1:
+                df = pd.read_json(json, orient="split", date_unit="ns")
+                df_list.append(df)
+    except Exception as e:
+        print("Failed to read dataframe from json")
+        print(e)
+    return df_list
+
+
+def lv_form(list_labels, list_values=None):
+    if list_values is None:
+        list_values = list_labels
+    return [{'label': label, 'value': value} for label, value in zip(list_labels,
+                                                                     list_values)]
+
+
+
+
+
+def duplicate_filename_check(filenames):
+    seen = set()
+    for filename in filenames:
+        filename = filename[0:31]
+        if filename in seen:
+            return True
+        seen.add(filename)
+    return False
+
+
+
+def remove_bad_files(children, filenames):
+    indices_to_remove = [index for index, child in enumerate(children) if child is None]
+    for index in sorted(indices_to_remove, reverse=True):
+        del filenames[index]
+    children = list(filter(None, children))
+    return children, filenames
+
+
+def _get_ds_dataframe(contents):
+    content_type, content_string = contents.split(",")
+    decoded = base64.b64decode(content_string)
+    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+    return df
+
+
+def remove_file_extension(filenames):
+    """ Removes .csv extension.
+
+    Args:
+        filenames: A list of filenames ending with .csv.
+
+    Returns: A list of filenames without their .csv extension.
+
+    """
+    return [re.sub(r"\.csv$", "", filename) for filename in filenames]
+
+
+def _check_filename(filename):
+    # Check file type
+    if 'csv' not in filename:
+        return False, filename
+
+    # Check for duplicate csv names
+    # It compares the first 31 characters since they are used for the excel sheet name.
+    # If there was duplicates then the excel sheet in the export would be overwritten
+    # for each duplicate drill schedule.
+    filename = re.sub(r"\.csv$", "", filename)
+    return True, filename
